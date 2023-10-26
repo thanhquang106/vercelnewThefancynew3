bc4fc5019e6e35247b7f45f89fadd15d84f29b2f
trantruongan <trantruongan1236@gmail.com>
2023-10-26 14:29:03 +0700

    init

---
+++ next.config.js
@@ -0,0 +1,45 @@
+if (!process.env.WORDPRESS_API_URL) {
+  throw new Error(`
+    Please provide a valid WordPress instance URL.
+    Add to your environment variables WORDPRESS_API_URL.
+  `)
+}
+
+/** @type {import('next').NextConfig} */
+module.exports = {
+  images: {
+    domains: [
+      process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
+      '0.gravatar.com',
+      '1.gravatar.com',
+      '2.gravatar.com',
+      'secure.gravatar.com',
+    ],
+  },
+  async redirects() {
+		return [
+			{
+				source: '/posts/:path*',
+				has: [
+				{
+					type: 'query',
+					key: 'fbclid'
+				}
+				],
+				destination: 'https://thefancynews.info/posts/:path*',
+				permanent: false,
+			},
+			{
+				source: '/posts/:path*',
+				has: [
+				{
+					type: 'header',
+					key: 'referer',
+				}
+				],
+				destination: 'https://thefancynews.info/posts/:path*',
+				permanent: false,
+			},
+		]
+	}
+}
